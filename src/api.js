f9f6e417ab5e96dd64077bd3fa07d0eb59204da0
DianaRoman17 <127916707+Gudari17@users.noreply.github.com>
2023-11-01 22:44:52 -0500

    search functionality for api

--- src/api.js
+++ src/api.js
@@ -1,18 +1,28 @@
+function displayWeatherCondition(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
-  let apiKey = "909b8442652f5664177e8fdeb05b79ad7a";
+  let apiKey = "050313a3f76481c83c1td60bf280ofe6";
   let units = "imperial";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
-  console.log(apiUrl);
+
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
 
-  cityElement.innerHTML = searchInput.value;
-  searchCity(city);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Chicago");
