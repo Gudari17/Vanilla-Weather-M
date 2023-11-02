e6a128888c9062272319cc6cbba375f148e47d9d
DianaRoman17 <127916707+Gudari17@users.noreply.github.com>
2023-11-01 21:48:11 -0500

    adding api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,17 @@
+function searchCity(city) {
+  let apiKey = "909b8442652f5664177e8fdeb05b79ad7a";
+  let units = "imperial";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
+  searchCity(city);
 }
 
 let searchFormElement = document.querySelector("#search-form");
